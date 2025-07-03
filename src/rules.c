1096d4586c16028b20a6eabba4d33e1f4ea19639
Goethe Emilio Arevalo Zuniga <garevalo@c1r8s3.42madrid.com>
2025-07-03 15:53:30 +0200

    starting with list, each node has a char

--- src/rules.c
+++ src/rules.c
@@ -6,8 +6,29 @@
 /*   By: garevalo <garevalo@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 14:31:19 by garevalo          #+#    #+#             */
-/*   Updated: 2025/07/03 14:31:25 by garevalo         ###   ########.fr       */
+/*   Updated: 2025/07/03 15:51:53 by garevalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+static typedef struct t_list
+{
+	const char	a;
+} t_stack;
+
+
+//rotate the first node, claiming the last position
+void	rotate(t_stack **stack, const char *rule)
+{
+	//i need aux list
+	//takes the first node
+	//find_last_node()
+	//create node and save the first
+	//now t_stack stack = aux_stack
+}
+
+void	push()
+{
+	
+}
 
 
