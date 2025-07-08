52b378eac3312722956e8b8cee4925a19d52e31b
Goethe Emilio Arevalo Zuniga <garevalo@c1r7s1.42madrid.com>
2025-07-08 15:21:58 +0200

    rotate, status : DOING (re-try)

--- src/rules.c
+++ src/rules.c
@@ -16,35 +16,8 @@
 #include "../libft/libft.h"
 #include "../includes/push_swap.h"
 
-//rotate the first node, claiming the last position. Eg. "0 1 2" -> "1 2 0"
-void	rotate(t_stacks *stacks, char *direction, int n)
-{
-	char *y_up;
-	char *y_down;
-
-	y_down = "down";
-	y_up = "up";
-	if (ft_strcmp(direction, y_up, ft_strlen(y_up)))
-	{
-		//rotate
-	}
-	else if (ft_strcmp(direction, y_down, ft_strlen(y_down)))
-	{
-		//reserve rorate
-	}
-	else
-	{
-		error_message();
-		clean_
-	}	
-}
-
-//push the first element from one stack to another. Eg. one. a: "0 1 6 0" , after push, "1 6 0". b: "0" 
-void push(t_stacks *stacks, const char *strnum)
+void		rotate(int *array, int size, char *direction, char *list)
 {
 }
-
-//swap the first element & the second one in the same stack.
-void	swap(t_stacks *stacks)
-{
-}
+//void		swap(char *str, int *array, int size);
+//void		push(char *str, t_stacks *s);
