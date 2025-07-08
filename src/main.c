dcec486e1786be61517fb8650d3963b5e378ce1d
byemidev <byemidev@gmail.com>
2025-07-08 14:30:18 +0200

    lets follow some project structure

--- src/main.c
+++ src/main.c
@@ -2,8 +2,28 @@
 //input error handling 
 //clean stacks and free memory
 
-int main (int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    //TODO: main logic
-    return (0);
+	t_stacks	*s;
+
+	validate_arguments(argc, argv);
+	s = malloc(sizeof * s);
+	if (s == NULL)
+		exit(1);
+	initialize_stacks(argc, argv, s);
+	join_args(argc, argv, s);
+	parse_numbers(s);
+	exit_if_sorted_or_has_duplicate(s, 0);
+	create_index(s);
+	if (s->a_size == 2 && s->a[0] > s->a[1])
+		swap("sa", s->a, s->a_size);
+	else if (s->a_size == 3)
+		sort_three_elements(s);
+	else if (s->a_size >= 4 && s->a_size <= 5)
+		sort_four_to_five_elements(s);
+	else
+		radix_sort(s);
+	exit_if_sorted_or_has_duplicate(s, 1);
+	free_and_exit_with_message(s, "Error\n");
+	return (0);
 }
--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,25 @@
 //all utils i could be need (no one exist in libft or another .c file)
 
+int input_isvalid(char **argv)
+{
+    int i;
+    char c;
+
+    if (argv[1][0] == '\0')
+        return (1);
+    i = 0;
+    while (argv[1][i])
+    {
+        c = argv[1][i];
+        if (!ft_isdigit(c))
+            return (1);
+        if (c == ' ')
+            i++;
+        i++;
+    }
+    return (0);
+}
+
 int *get_max(int arr[], int n)//pass the array and the max number of values in
 {
   //getting max number of elements in the array
@@ -7,8 +27,7 @@ int *get_max(int arr[], int n)//pass the array and the max number of values in
 
 void    error_message(char *message)
 {
-    //there is message ? yes, print it.
-    //no default message output.
+    ft_putstr_fd(message, 2);
 }
 void    auto_clean(t_stacks *stacks)
 {
