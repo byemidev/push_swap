30604b28e0ccd4fb0ef4b95248b9a7686cd1e001
byemidev <byemidev@gmail.com>
2025-07-10 23:00:24 +0200

    rules finished no tested

--- src/rules.c
+++ src/rules.c
@@ -10,14 +10,75 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-//rules
-
-//TODO: what headers i need
 #include "../libft/libft.h"
 #include "../includes/push_swap.h"
 
-void		rotate(int *array, int size, char *direction, char *list)
+//swap first value and second onthe
+void	swap(char *str, int *array, int size)
 {
+	int	tmp;
+
+	if (size <= 0)
+		return ;
+	tmp = array[0];
+	array[0] = array[1];
+	array[1] = tmp;
+	ft_putendl_fd(str, 1);
 }
-//void		swap(char *str, int *array, int size);
-//void		push(char *str, t_stacks *s);
+
+//push the first value from one stack to another one
+void	push(char *str, t_stacks *s)
+{
+	int	tmp;
+
+
+	if (ft_strcmp(str, "pa", 3))
+	{
+		//push_b_to_a
+		if (s->b_size <= 0)
+			return ;
+		tmp = s->b[0];
+		ft_memmove(s->a + 1, s->a, sizeof(int) * s->a_size);
+		s->a[0] = tmp;
+		s->b_size--;
+		ft_memmove(s->b, s->b +1, sizeof(int) * s->b_size);
+		s->a_size++;
+	}
+	else
+	{
+		//push_a_to_b
+		if (s->a_size <= 0)
+			return ;
+		tmp = s->a[0];
+		ft_memmove(s->b + 1, s->b, sizeof(int) * s->b_size);
+		s->b[0] = tmp;
+		s->a_size--;
+		ft-memmove(s->a, s->b +1; sizeof(int) * s->a_size);
+		s->b_size++;
+	}
+	ft_putendl_fd(str, 1);
+}
+
+//rotate first and last element.Depends which direction takes.
+void	rotate(int *array, int size, char *direction, char *list)
+{
+	//directions change the flow.
+	int	tmp;
+
+	if (size < 0)
+		return ;
+	if (ft_strcmp(direction, "up", 5) == 0)
+	{
+		tmp = array[0];
+		ft_memmove(array, array + 1, sizeof(int) * (size -1));
+		array[size - 1] = tmp;
+		write(1, "r", 1);
+	}
+	else if (ft_strcmp(direction, "down", 5) == 0)
+	{
+		tmp = array[size - 1];
+		ft_memmove(array + 1, array, sizeof(int) * (size -1));
+		array[0] = tmp;
+		write(1, "rr", 2);
+	}
+}
